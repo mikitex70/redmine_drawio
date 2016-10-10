462508e3a5727ad89e149f2566fba19c0ad656e4
Michele Tessaro <michele.tessaro@email.it>
2016-10-10 19:58:50 +0200

    fix: usr: fixed missing end of module

--- lib/hooks/view_hooks.rb
+++ lib/hooks/view_hooks.rb
@@ -1,12 +1,14 @@
 module RedmineDrawio
   class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
       
-  def view_layouts_base_body_bottom(context={})
-    if context[:controller] && (context[:controller].is_a?(IssuesController) ||
-                                    context[:controller].is_a?(WikiController))
-      return javascript_tag(nil, src: "https://www.draw.io/js/viewer.min.js")
-    else
-      return ''
+    def view_layouts_base_body_bottom(context={})
+      if context[:controller] && (context[:controller].is_a?(IssuesController) ||
+                                  context[:controller].is_a?(WikiController))
+        return javascript_tag(nil, src: "https://www.draw.io/js/viewer.min.js")
+      else
+        return ''
+      end
     end
   end
 end
+
