aadffa6eed696ed38d0f92251d1a6c19b34f4333
Michele Tessaro <michele.tessaro@email.it>
2016-10-12 20:07:07 +0200

    fix: usr: fixed rendering of some graphs (like flowcharts)

--- lib/hooks/view_hooks.rb
+++ lib/hooks/view_hooks.rb
@@ -4,7 +4,7 @@ module RedmineDrawio
     def view_layouts_base_body_bottom(context={})
       if context[:controller] && (context[:controller].is_a?(IssuesController) ||
                                   context[:controller].is_a?(WikiController))
-        return javascript_tag(nil, src: "https://www.draw.io/js/viewer.min.js")
+        return javascript_tag(nil, src: "https://www.draw.io/embed2.js?s=general;flowchart;bpmn;lean_mapping;electrical;pid;rack;ios;aws2;azure;cisco;clipart;signs;uml;er;mockups")
       else
         return ''
       end
