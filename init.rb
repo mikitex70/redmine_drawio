cca961856c70db98c0b88adcf9ab802b411de352
Pavel Rosický <pavel.rosicky@easy.cz>
2017-05-05 11:31:07 +0200

    fix: dev: fix typo

--- init.rb
+++ init.rb
@@ -5,15 +5,15 @@ Redmine::Plugin.register :redmine_drawio do
   version '0.6.0'
   url 'https://github.com/mikitex70/redmine_drawio'
   author_url 'https://github.com/mikitex70'
-  
+
   requires_redmine version: '2.6'..'3.3'
-  
+
   settings(partial: 'settings/drawio_settings',
            default: {'drawio_service_url' => '//www.draw.io'})
 
-  should_be_disabled false if Redmine::Plugin.installed?(:easy_extension)
+  should_be_disabled false if Redmine::Plugin.installed?(:easy_extensions)
 end
 
-unless Redmine::Plugin.installed?(:easy_extension)
-    require_relative 'after_init'
+unless Redmine::Plugin.installed?(:easy_extensions)
+  require_relative 'after_init'
 end
