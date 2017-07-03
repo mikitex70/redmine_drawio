8367ea6b826ad53decf2f5e1fef2ae6d096d2c45
Denis Sidorov <sidorovdi@gmail.com>
2017-07-03 16:24:24 +0500

    new: usr: added russian translation
    Russian for drawio_jstoolbar

---
+++ assets/javascripts/lang/drawio_jstoolbar-ru.js
@@ -0,0 +1,22 @@
+// General messages
+Drawio.strings['drawio_updating_page'] = 'Обновление страницы';
+Drawio.strings['drawio_error_saving' ] = 'Ошибка при сохранении диаграммы:\n';
+Drawio.strings['drawio_http_401'     ] = 'Не авторизован';
+Drawio.strings['drawio_http_404'     ] = 'Убедитесь, что WebDAV для модуля DMSF включен';
+Drawio.strings['drawio_http_409'     ] = 'Убедитесь, что DMSF папка существует и доступна';
+Drawio.strings['drawio_http_422'     ] = 'Размер диаграммы слишком большой: увеличьте максимальный размер вложений в настройках Redmine';
+Drawio.strings['drawio_http_502'     ] = 'Убедитесь, что WebDAV для модуля DMSF включен в режиме чтения/записи';
+// CKEditor plugin messages
+Drawio.strings['drawio_cke_diagName'       ] = 'Название диаграммы';
+Drawio.strings['drawio_cke_diagType'       ] = 'Формат диаграммы';
+Drawio.strings['drawio_cke_size'           ] = 'Ширина (в пикселах)';
+Drawio.strings['drawio_cke_attach_dlgtitle'] = 'Определить прикрепленную диаграмму';
+Drawio.strings['drawio_cke_attach_btnlabel'] = 'Прикрепленная Drawio-диаграмма';
+Drawio.strings['drawio_cke_dmsf_dlgtitle'  ] = 'Определить DMSF-диаграмму';
+Drawio.strings['drawio_cke_dmsf_btnlabel'  ] = 'Drawio DMSF-диаграмма';
+
+// Standard jstoolbar editor messages
+Drawio.strings['drawio_btn_ok'      ] = 'Вставить макрос';
+Drawio.strings['drawio_btn_cancel'  ] = 'Отмена';
+Drawio.strings['drawio_attach_title'] = 'Прикрепить к Drawio-диаграмме';
+Drawio.strings['drawio_dmsf_title'  ] = 'Использовать Drawio-диаграмму, сохраненную в DMSF-модуле';
