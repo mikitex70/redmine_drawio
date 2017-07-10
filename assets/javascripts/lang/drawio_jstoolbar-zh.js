02ab938d4e97f1d6bc3b93e398591a850028846c
Steven.W@UTH <archonwang1981@gmail.com>
2017-07-11 02:55:25 +0800

    chg: usr: translated messages for Simplifed Chinese
    Translation of file drawio_jstoolbar-zh.js into Simplified Chinese.

--- assets/javascripts/lang/drawio_jstoolbar-zh.js
+++ assets/javascripts/lang/drawio_jstoolbar-zh.js
@@ -1,22 +1,22 @@
 // General messages
-Drawio.strings['drawio_updating_page'] = 'Updating page';
-Drawio.strings['drawio_error_saving' ] = 'Error saving diagram:\n';
-Drawio.strings['drawio_http_401'     ] = 'Not authorized';
-Drawio.strings['drawio_http_404'     ] = 'Make sure WebDAV capabilities of DMSF module is enabled';
-Drawio.strings['drawio_http_409'     ] = 'Make sure the DMSF folder exists and is accessible';
-Drawio.strings['drawio_http_422'     ] = 'Diagram size too big: increase the attachment size in Redmine settings';
-Drawio.strings['drawio_http_502'     ] = 'Make sure WebDAV capabilities of DMSF module is enabled in Read/Write mode';
+Drawio.strings['drawio_updating_page'] = '正在更新页面';
+Drawio.strings['drawio_error_saving' ] = '保存图形报错：\n';
+Drawio.strings['drawio_http_401'     ] = '未经授权';
+Drawio.strings['drawio_http_404'     ] = '请确保 DMSF 的 WebDAV 功能已启用';
+Drawio.strings['drawio_http_409'     ] = '请确保 DMSF 文件夹已创建并可被访问';
+Drawio.strings['drawio_http_422'     ] = '图形尺寸过大：请在 Redmine 的设置中扩大附件容量';
+Drawio.strings['drawio_http_502'     ] = '请确保 DMSF 的 WebDAV 功能已启用读/写模式';
 // CKEditor plugin messages
-Drawio.strings['drawio_cke_diagName'       ] = 'Diagram name';
-Drawio.strings['drawio_cke_diagType'       ] = 'Diagram format';
-Drawio.strings['drawio_cke_size'           ] = 'Width (pixels)';
-Drawio.strings['drawio_cke_attach_dlgtitle'] = 'Define attached diagram';
-Drawio.strings['drawio_cke_attach_btnlabel'] = 'Drawio attached diagram';
-Drawio.strings['drawio_cke_dmsf_dlgtitle'  ] = 'Define DMSF diagram';
-Drawio.strings['drawio_cke_dmsf_btnlabel'  ] = 'Drawio DMSF diagram';
+Drawio.strings['drawio_cke_diagName'       ] = '图形名称';
+Drawio.strings['drawio_cke_diagType'       ] = '图形格式';
+Drawio.strings['drawio_cke_size'           ] = '宽度（像素）';
+Drawio.strings['drawio_cke_attach_dlgtitle'] = '定义附图';
+Drawio.strings['drawio_cke_attach_btnlabel'] = 'Drawio 附图';
+Drawio.strings['drawio_cke_dmsf_dlgtitle'  ] = '定义 DMSF 图形';
+Drawio.strings['drawio_cke_dmsf_btnlabel'  ] = 'Drawio DMSF 图形';
 
 // Standard jstoolbar editor messages
-Drawio.strings['drawio_btn_ok'      ] = 'Insert macro';
-Drawio.strings['drawio_btn_cancel'  ] = 'Cancel';
-Drawio.strings['drawio_attach_title'] = 'Attach a Drawio diagram';
-Drawio.strings['drawio_dmsf_title'  ] = 'Use Drawio diagram stored in DMSF module';
+Drawio.strings['drawio_btn_ok'      ] = '插入宏命令';
+Drawio.strings['drawio_btn_cancel'  ] = '取消';
+Drawio.strings['drawio_attach_title'] = '附上 Drawio 图形';
+Drawio.strings['drawio_dmsf_title'  ] = '使用存储于 DMSF 模块中的 Drawio 图形';
