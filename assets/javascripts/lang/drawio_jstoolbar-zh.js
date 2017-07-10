a1e1ad73b1434ea4bb932ba4f5f178742a19b1d1
Michele Tessaro <michele.tessaro@email.it>
2017-07-10 18:25:13 +0200

    fix: dev: added toolbar language placeholder for Simplified Chinese
    Added language file for Simplified Chinese. Messages are in English and
    must be translated, but at least it let possibile to checkout a working
    version.

---
+++ assets/javascripts/lang/drawio_jstoolbar-zh.js
@@ -0,0 +1,22 @@
+// General messages
+Drawio.strings['drawio_updating_page'] = 'Updating page';
+Drawio.strings['drawio_error_saving' ] = 'Error saving diagram:\n';
+Drawio.strings['drawio_http_401'     ] = 'Not authorized';
+Drawio.strings['drawio_http_404'     ] = 'Make sure WebDAV capabilities of DMSF module is enabled';
+Drawio.strings['drawio_http_409'     ] = 'Make sure the DMSF folder exists and is accessible';
+Drawio.strings['drawio_http_422'     ] = 'Diagram size too big: increase the attachment size in Redmine settings';
+Drawio.strings['drawio_http_502'     ] = 'Make sure WebDAV capabilities of DMSF module is enabled in Read/Write mode';
+// CKEditor plugin messages
+Drawio.strings['drawio_cke_diagName'       ] = 'Diagram name';
+Drawio.strings['drawio_cke_diagType'       ] = 'Diagram format';
+Drawio.strings['drawio_cke_size'           ] = 'Width (pixels)';
+Drawio.strings['drawio_cke_attach_dlgtitle'] = 'Define attached diagram';
+Drawio.strings['drawio_cke_attach_btnlabel'] = 'Drawio attached diagram';
+Drawio.strings['drawio_cke_dmsf_dlgtitle'  ] = 'Define DMSF diagram';
+Drawio.strings['drawio_cke_dmsf_btnlabel'  ] = 'Drawio DMSF diagram';
+
+// Standard jstoolbar editor messages
+Drawio.strings['drawio_btn_ok'      ] = 'Insert macro';
+Drawio.strings['drawio_btn_cancel'  ] = 'Cancel';
+Drawio.strings['drawio_attach_title'] = 'Attach a Drawio diagram';
+Drawio.strings['drawio_dmsf_title'  ] = 'Use Drawio diagram stored in DMSF module';
