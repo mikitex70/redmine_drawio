201b55cf491fc4c1ac1d1d5750987a9f81dfe50f
Michele Tessaro <michele.tessaro@email.it>
2017-07-17 20:18:33 +0200

    fix: usr: fixed https access to the drawio site (fixes #41)
    Fixed a wrongly disabled https protocol when calling the www.draw.io
    site, which caused security problems with modern browsers.

--- assets/javascripts/drawioEditor.js
+++ assets/javascripts/drawioEditor.js
@@ -135,7 +135,7 @@ function editDiagram(image, resource, isDmsf, pageName) {
     };
     
     // Disables SSL if the protocol isn't HTTPS; simplifies use of local drawio installations
-    var useHttps = (Drawio.settings.drawioUrl.match(/^https:/i)? 1: 0);
+    var useHttps = (Drawio.settings.drawioUrl.match(/^(https:)?\/\//i)? 1: 0);
 
     window.addEventListener('message', receive);
     iframe.setAttribute('src', Drawio.settings.drawioUrl+'?embed=1&ui=atlas&spin=1&modified=unsavedChanges&proto=json&https='+useHttps);
