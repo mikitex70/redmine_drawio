1657a50e0b1c224b025fea750b0a82f840f437bb
Michele Tessaro <michele.tessaro@email.it>
2017-12-29 19:04:55 +0100

    fix: dev: removed prepend to avoid errors with other plugins (refs #44)
    The 'prepend' method cannot be used to replace the 'alias_method_chain'
    as may generate 'stack level too deep' errors in conjunction with other,
    not ported, plugins.
    As a workaround the 'alias_method_chain' has been replaced by a couple
    of 'alias_method'.

--- lib/redmine_drawio/helpers/textile_helper.rb
+++ lib/redmine_drawio/helpers/textile_helper.rb
@@ -3,7 +3,7 @@ require 'redmine'
 
 module Redmine::WikiFormatting::Textile::Helper
     def heads_for_wiki_formatter_with_drawio
-        super #heads_for_wiki_formatter_without_drawio
+        heads_for_wiki_formatter_without_drawio
         unless @heads_for_wiki_formatter_with_drawio_included
             # This code is executed only once and inserts a javascript code
             # that patches the jsToolBar adding the new buttons.
@@ -15,8 +15,10 @@ module Redmine::WikiFormatting::Textile::Helper
         end
     end
     
-    # for details see
-    #   https://www.justinweiss.com/articles/rails-5-module-number-prepend-and-the-end-of-alias-method-chain/
-    #
+    # alias_method_chain is deprecated in Rails 5: replaced with two alias_method
+    # as a quick workaround. Using the 'prepend' method can generate an
+    # 'stack level too deep' error in conjunction with other (non ported) plugins.
     #alias_method_chain :heads_for_wiki_formatter, :drawio
+    alias_method :heads_for_wiki_formatter_without_drawio, :heads_for_wiki_formatter
+    alias_method :heads_for_wiki_formatter, :heads_for_wiki_formatter_with_drawio
 end
